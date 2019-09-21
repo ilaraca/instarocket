ff7d69077f7851432bf09c4169f48bd9db0e7b9e
Ilara Almeida <ilaraalmeida@MBP-de-Ilara.localdomain>
2019-09-21 15:41:17 -0300

    refactor(js): refactor Feed.js done

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -21,7 +21,38 @@ class Feed extends Component {
                     </header>
                     <img src="http://localhost:3333/files/Screen Shot 2019-08-28 at 21.jpg" />
                     <footer>
-                        
+                        <div className="actions"> 
+                            <img src={like} alt=""/>
+                            <img src={comment} alt=""/>
+                            <img src={send} alt=""/>
+                        </div>
+                        <strong>900 curtidas</strong>
+                        <p>
+                            Ilara é muito gata
+                            <span>#ilaragata#gatafoda</span>
+                        </p>
+                    </footer>
+                </article>
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Ilara Almeida</span>
+                            <span className="place"> São Paulo</span>
+                        </div>
+                        <img src={more} alt="Mais"/>
+                    </header>
+                    <img src="http://localhost:3333/files/Screen Shot 2019-08-28 at 21.jpg" />
+                    <footer>
+                        <div className="actions"> 
+                            <img src={like} alt=""/>
+                            <img src={comment} alt=""/>
+                            <img src={send} alt=""/>
+                        </div>
+                        <strong>900 curtidas</strong>
+                        <p>
+                            Ilara é muito gata
+                            <span>#ilaragata#gatafoda</span>
+                        </p>
                     </footer>
                 </article>
             </section>
