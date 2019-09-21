4cc78f83c475f26749f88fcfa2c5019b4411a071
Ilara Almeida <ilaraalmeida@MBP-de-Ilara.localdomain>
2019-09-21 12:15:56 -0300

    folder(feat): add frontend folder

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+// import { Container } from './styles';
+
+export default function Header() {
+  return (
+      <header id='main-header'>
+        <div className='header-content'>
+            <img src='' alt='InstaRock' />
+            <img src='' alt='Enviar Publicação'/>
+        </div>
+      </header>
+  );
+}
